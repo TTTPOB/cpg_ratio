e801d2380cb78e8deef6588c8ccb32f76c9cf99e
yujw <yu-jw20@mails.tsinghua.edu.cn>
2023-02-03 19:06:40 +0800

    refractor logic, remove unused code, and fix bug related to chrom end

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use bio::io::fasta::{self, Record};
-use clap::{Parser, Subcommand, ValueEnum};
+use clap::{Parser, Subcommand};
 use std::io::{self, prelude::*, BufWriter};
 
 #[derive(Parser)]
@@ -25,6 +25,30 @@ struct CgArgs {
 struct CpGArgs {
     #[clap(short, long)]
     window_size: u64,
+}
+#[derive(Debug, Clone, Copy)]
+enum ScoreTypes {
+    CpG,
+    CG,
+}
+trait Scoring {
+    fn score(&self, seq: &[u8]) -> f64;
+}
+impl Scoring for ScoreTypes {
+    fn score(&self, seq: &[u8]) -> f64 {
+        match self {
+            ScoreTypes::CpG => {
+                let cg_count = count_bigram(seq, b"CG");
+                let total_bigram_count = seq.len() as u64 - 1;
+                (cg_count as f64) / (total_bigram_count as f64)
+            }
+            ScoreTypes::CG => {
+                let cg_count = count_base(seq, b"CG");
+                let total_base_count = seq.len() as u64;
+                (cg_count as f64) / (total_base_count as f64)
+            }
+        }
+    }
 }
 
 struct BdgRecord {
@@ -65,64 +89,47 @@ fn count_base(seq: &[u8], bases: &[u8]) -> u64 {
     seq.iter().filter(|b| bases.contains(b)).count() as u64
 }
 
-struct CpGCounter {
+struct CGCounter<T: Scoring> {
     record: Record,
     window_size: u64,
     current_idx: usize,
+    score_type: T,
 }
-
-impl Iterator for CpGCounter {
-    type Item = BdgRecord;
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.current_idx > self.record.seq().len() {
-            return None;
-        }
-        let start = self.current_idx;
-        let end = std::cmp::min(
+impl<T: Scoring> CGCounter<T> {
+    fn start(&self) -> usize {
+        self.current_idx
+    }
+    fn end(&self) -> usize {
+        std::cmp::min(
             self.current_idx + self.window_size as usize,
             self.record.seq().len(),
-        );
-        let seq = &self.record.seq()[start..end];
-        let cpg_count = count_bigram(seq, b"CG");
-        let total_bigram_count = seq.len() - 1;
-        let score = (cpg_count as f64) / (total_bigram_count as f64);
+        )
+    }
+    fn step(&mut self) {
         self.current_idx += self.window_size as usize;
-        Some(BdgRecord::new(
-            self.record.id().to_string(),
-            start as u64,
-            end as u64,
-            score,
-        ))
+    }
+    fn seq(&self) -> &[u8] {
+        &self.record.seq()[self.start()..self.end()]
+    }
+    fn score(&self) -> f64 {
+        self.score_type.score(self.seq())
     }
 }
 
-struct CGCounter {
-    record: Record,
-    window_size: u64,
-    current_idx: usize,
-}
-impl Iterator for CGCounter {
+impl<T: Scoring> Iterator for CGCounter<T> {
     type Item = BdgRecord;
     fn next(&mut self) -> Option<Self::Item> {
-        if self.current_idx > self.record.seq().len() {
+        if self.start() >= self.record.seq().len() {
             return None;
         }
-        let start = self.current_idx;
-        let end = std::cmp::min(
-            self.current_idx + self.window_size as usize,
-            self.record.seq().len(),
-        );
-        let seq = &self.record.seq()[start..end];
-        let cg_count = count_base(seq, b"CG");
-        let total_base_count = seq.len();
-        let score = (cg_count as f64) / (total_base_count as f64);
-        self.current_idx += self.window_size as usize;
-        Some(BdgRecord::new(
+        let bdg = BdgRecord::new(
             self.record.id().to_string(),
-            start as u64,
-            end as u64,
-            score,
-        ))
+            self.start() as u64,
+            self.end() as u64,
+            self.score(),
+        );
+        self.step();
+        Some(bdg)
     }
 }
 
@@ -133,27 +140,22 @@ fn main() {
     let fasta = fasta::Reader::new(input);
     let mut records = fasta.records();
     while let Some(Ok(record)) = records.next() {
-        match app.command {
-            Commands::Cg(args) => {
-                let mut counter = CGCounter {
-                    record,
-                    window_size: args.window_size,
-                    current_idx: 0,
-                };
-                while let Some(record) = counter.next() {
-                    record.writeln(&mut output).unwrap();
-                }
-            }
-            Commands::CpG(args) => {
-                let mut counter = CpGCounter {
-                    record,
-                    window_size: args.window_size,
-                    current_idx: 0,
-                };
-                while let Some(record) = counter.next() {
-                    record.writeln(&mut output).unwrap();
-                }
-            }
+        let bdg_records = match &app.command {
+            &Commands::CpG(args) => CGCounter {
+                record,
+                window_size: args.window_size,
+                current_idx: 0,
+                score_type: ScoreTypes::CpG,
+            },
+            &Commands::Cg(args) => CGCounter {
+                record,
+                window_size: args.window_size,
+                current_idx: 0,
+                score_type: ScoreTypes::CG,
+            },
+        };
+        for bdg in bdg_records {
+            bdg.writeln(&mut output).unwrap();
         }
     }
 }
